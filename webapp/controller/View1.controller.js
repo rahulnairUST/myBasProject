56f993d9e98fefc19eabb7b602ff57002a9efa21
Rahul Nair <andhur.rahulnair@ust.com>
2023-08-10 06:55:14 +0000

    first commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("project1.controller.View1", {
+            onInit: function () {
+                var oModel = this.getOwnerComponent().getModel();
+                var oNote = oModel.bindContext('/FLEXIOM');
+                debugger;
+                // oNote.requestValue().then(function (sValue) {
+                //     debugger;
+                // });
+            }
+        });
+    });
